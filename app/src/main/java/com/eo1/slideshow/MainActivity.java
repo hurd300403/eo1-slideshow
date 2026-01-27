93154452c25ff2d084fffac814612790f870d6f8
sam hurd <samhurd@gmail.com>
2026-01-26 21:11:28 -0500

    Device: living-room (default)

--- app/src/main/java/com/eo1/slideshow/MainActivity.java
+++ app/src/main/java/com/eo1/slideshow/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends Activity {
 
     private WebView webView;
     private Handler handler;
-    private static final String URL = "http://93.127.216.80:3000/d/office";
+    private static final String URL = "http://93.127.216.80:3000/d/living-room";
     private static final int RETRY_DELAY = 5000; // 5 seconds
     private static final int WIFI_CHECK_DELAY = 2000; // 2 seconds
     private boolean pageLoaded = false;
